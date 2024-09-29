59eda640942293674fb0364eb7caf9a8c620d938
Hajid Raihan <hajidraihan@gmail.com>
2024-09-29 23:55:43 +0800

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,148 @@
+"use client";
+
 import Image from "next/image";
+import Typewriter from "typewriter-effect";
+import Foto from "../asset/foto.png";
+import Blob from "../asset/blob.svg";
+import Html from "../asset/html.svg";
+import Css from "../asset/css.svg";
+import Javascript from "../asset/javascript.svg";
+import Next from "../asset/next.svg";
+import ReactIcon from "../asset/react.svg";
+import Node from "../asset/node.svg";
+import Laravel from "../asset/laravel.svg";
+import Mongo from "../asset/mongo.svg";
+import Postgres from "../asset/postgres.svg";
+import Tailwind from "../asset/tailwind.svg";
+import Typescript from "../asset/typescript.svg";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <>
+      <Header />
+      <div className="bg-black ">
+        <About />
+        {/* <Skill /> */}
+      </div>
+    </>
   );
 }
+
+const Header = () => {
+  return (
+    <div className="py-10 w-full flex px-32 items-center bg-[#F5F5F5] justify-between">
+      <div className="w-1/2   justify-center items-center">
+        <div className="">
+          <h1 className="text-[3rem] font-semibold gradient">
+            {/* <Typewriter
+            onInit={(typewriter) => {
+              typewriter
+                .typeString("Hello.")
+                .pauseFor(1000)
+                .deleteAll()
+                .typeString("Welcome to my website!")
+                .start();
+            }}
+            options={{
+              // loop: true,
+              cursor: "|",
+              delay: 100,
+            }}
+          /> */}
+            Hi. I'm Hajid Raihan
+          </h1>
+          {/* <h1 className="text-[3rem] font-semibold gradient">Hajid Raihan</h1> */}
+          <h1 className="text-[2rem] font-semibold gradient">A Passionate Software Developer</h1>
+          <button className="py-2 px-5 bg-black text-white rounded- text-md mt-5 button-shadow">
+            Contact me
+          </button>
+        </div>
+      </div>
+
+      <div className="w-1/2">
+        <div className="w-96 h-[500px] ms-20 bg-[#6a6875] rounded-full">
+          {/* <Image
+          src={Blob}
+          alt="Profile Image"
+          className="absolute z-10 w-[500px] h-[500px] object-cover rounded-full bottom-0 left-1/2 transform -translate-x-1/2"
+        /> */}
+
+          {/* Profile image */}
+          <Image
+            src={Foto}
+            alt="Profile Image"
+            className="w-full h-full object-cover rounded-full"
+          />
+        </div>
+      </div>
+
+      {/* <button className="py-2 px-5 bg-black text-white rounded-lg text-xl">scroll</button> */}
+    </div>
+  );
+};
+
+const About = () => {
+  return (
+    <div className="py-10 w-full  px-32 items-center text-white justify-between">
+      <h1 className="text-[3rem]">About</h1>
+      <div className="flex justify-between w-full">
+        <p className="text-justify w-1/2 pe-10">
+          I'm an undergraduated student at Hasanuddin University with System Information Major. I
+          recently began my journey as a software developer and am excited about the endless
+          possibilities that coding provides. I've completed courses in html, css, javascript,
+          Node.js, React.js, Next.js, Express.js, MongoDB, little bit Laravel and have started
+          building small projects to sharpen my skills. My goal is to contribute to impactful
+          projects and continue expanding my knowledge in both front-end and back-end development.
+        </p>
+        <div className="w-1/2">
+          <div className="flex gap-3 flex-wrap ps-10">
+            <SkillCard icon={Html} />
+            <SkillCard icon={Css} />
+            <SkillCard icon={Javascript} />
+            <SkillCard icon={ReactIcon} />
+            {/* <SkillCard icon={Next} /> */}
+            <SkillCard icon={Node} />
+            <SkillCard icon={Mongo} />
+            <SkillCard icon={Laravel} />
+            <SkillCard icon={Postgres} />
+            <SkillCard icon={Tailwind} />
+            <SkillCard icon={Typescript} />
+          </div>
+        </div>
+      </div>
+
+      {/* <button className="py-2 px-5 bg-black text-white rounded-lg text-xl">scroll</button> */}
+    </div>
+  );
+};
+
+const Skill = () => {
+  return (
+    <div className="w-full px-32 items-center text-white justify-between">
+      {/* <h1 className="text-[3rem]">Languages / Frameworks /Tools </h1> */}
+      <div className="flex gap-3 flex-wrap justify-">
+        <SkillCard icon={Html} />
+        <SkillCard icon={Css} />
+        <SkillCard icon={Javascript} />
+        <SkillCard icon={ReactIcon} />
+        <SkillCard icon={Next} />
+        <SkillCard icon={Node} />
+        <SkillCard icon={Mongo} />
+        <SkillCard icon={Laravel} />
+        <SkillCard icon={Postgres} />
+        <SkillCard icon={Tailwind} />
+        <SkillCard icon={Typescript} />
+      </div>
+
+      {/* <button className="py-2 px-5 bg-black text-white rounded-lg text-xl">scroll</button> */}
+    </div>
+  );
+};
+
+const SkillCard = ({ icon }: { icon: string }) => {
+  return (
+    <div className="w-20 h-20 p-5 flex justify-center items-center bg-[#121212] button-shadow rounded-md">
+      <Image src={icon} alt="" className="w-full h-full" />
+    </div>
+  );
+};
